3351749fdeb43b74bfc2e1c07d11fbd875925aad
marrklouiee-ui <marrklouiee@gmail.com>
2026-08-28 20:42:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,31 @@ function showProject(projectName) {
     );
 
 }
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let currentSection = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+
+        if (window.scrollY >= sectionTop) {
+            currentSection = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + currentSection) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
