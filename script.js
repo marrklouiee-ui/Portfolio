f01e2d75e7e8dd106c202504733f2da34650dba2
marrklouiee-ui <marrklouiee@gmail.com>
2026-08-28 21:21:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -76,3 +76,15 @@ window.addEventListener("scroll", () => {
 
 });
 
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", function () {
+
+        navLinks.forEach(nav => {
+            nav.classList.remove("active");
+        });
+
+        this.classList.add("active");
+    });
+});
