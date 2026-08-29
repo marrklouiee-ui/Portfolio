230be50edcc1cbb5be4007cef7b35058cdd233b3
marrklouiee-ui <marrklouiee@gmail.com>
2026-08-29 18:59:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll("nav a");
+const navLinks = document.querySelectorAll(".nav-links a");
 
 window.addEventListener("scroll", () => {
 
-    let current = "";
+    let currentSection = "";
 
     sections.forEach(section => {
+
         const sectionTop = section.offsetTop;
         const sectionHeight = section.offsetHeight;
 
-        if (window.scrollY >= sectionTop - 200) {
-            current = section.getAttribute("id");
+        if (
+            window.scrollY >= sectionTop - 250 &&
+            window.scrollY < sectionTop + sectionHeight - 250
+        ) {
+            currentSection = section.id;
         }
+
     });
 
     navLinks.forEach(link => {
+
         link.classList.remove("active");
 
-        if (link.getAttribute("href") === "#" + current) {
+        if (link.getAttribute("href") === "#" + currentSection) {
             link.classList.add("active");
         }
+
     });
 
 });
-function openInfoModal() {
-    document.getElementById("infoModal").style.display = "flex";
-}
-
-function closeInfoModal() {
-    document.getElementById("infoModal").style.display = "none";
-}
